0ac2ec42ca51c02d965bbd49e65a21c794207140
XxMicrowavexX <76092869+XxMicrowavexX@users.noreply.github.com>
2021-03-17 08:15:22 +1300

    Added Rigidbody to player + 0g

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -7,21 +7,18 @@ var vec = Vector2()
 func get_input(delto):
 	if Input.is_action_pressed('ui_right'):
 		$Animation.play("Move")
-		linear_velocity.x += 1
+		add_force(Vector2(5, 0), Vector2(1, 0))
 	elif Input.is_action_pressed('ui_left'):
 		$Animation.play("Move")
-		linear_velocity.x -= 1
+		add_force(Vector2(5, 0), Vector2(-1, 0))
 	elif Input.is_action_pressed('ui_down'):
 		$Animation.play("Move")
-		linear_velocity.y += 1
+		add_force(Vector2(0, 5), Vector2(0, 1))
 	elif Input.is_action_pressed('ui_up'):
 		$Animation.play("Move")
-		linear_velocity.y -= 10
+		add_force(Vector2(0, 5), Vector2(0, -1))
 	else:
 		$Animation.play("Idle")
-		linear_velocity.y += 1
-	linear_velocity = linear_velocity.normalized() * speed
 
 func _process(delta):
-	linear_velocity = Vector2()
 	get_input(delta)
